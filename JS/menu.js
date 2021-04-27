cc68234aa90a1a13b5468f04641d16a17e91c011
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2021-04-26 21:55:39 -0700

    hamburger menu has been added to header.Animations have been added to menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const menuBtn = document.querySelector('.menu-btn');
+const hamBtn = document.querySelector('.burger-btn');
+const navigation = document.querySelector('.navigation');
+const navMenu = document.querySelector('.nav-menu');
+const items = document.querySelectorAll('.nav-menu__item');
+
+function toggleMenu() {
+  hamBtn.classList.toggle('open');
+  navigation.classList.toggle('open');
+  navMenu.classList.toggle('open');
+  items.forEach((item) => item.classList.toggle('open'));
+}
+
+menuBtn.addEventListener('click', toggleMenu);
