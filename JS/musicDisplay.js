8380a309a6a9839f0b2d96b861b676883135d2c1
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2021-05-10 15:37:11 -0700

    html template for audio player has been created

--- JS/musicDisplay.js
+++ JS/musicDisplay.js
@@ -2,30 +2,55 @@ import { tracksData } from './spotifyDataReq.js'
 
 const musicDisplayContainer = document.querySelector('.music-display');
 
-const trackImage = (imageUrl) => {
-  const imageElement = document.createElement('img');
-
-  imageElement.src = imageUrl;
-  return musicDisplayContainer.append(imageElement)
-}
 
 const trackPlayer = (trackInfo) => {
-  const trackInfoContainer = document.createElement('div');
-  const trackName = document.createElement('h2')
-  const artist = document.createElement()
+  trackInfo.map((track => {
+    const playerContainer = document.createElement('div');
+    const trackDisplayContainer = document.createElement('div');
+    const trackDisplay = document.createElement('div');
+    const albumCoverContainer = document.createElement('div');
+    const trackInfoContainer = document.createElement('div');
+
+    const ulControls = document.createElement('ul');
+    const liSongName = document.createElement('li');
+    const liArtist = document.createElement('li');
+
+    const imageElement = document.createElement('img');
+    const trackName = document.createElement('h2');
+    const artist = document.createElement('h3');
+    const album = document.createElement('h4');
+
+    playerContainer.innerHTML = `
+      <audio id='audio-player' ontimeupdate='seekBar() ondurationchange='CreateTrackSeekBar()' preload='auto'>
+        <source src=''https://p.scdn.co/mp3-preview/25afd2d1ae43f4e90083b921d02ea793b4966518?cid=26cef5c931754e4b85f90b16b2f9b3e3' type='audio/mpeg'/>
+      </audio>`;
 
 
+
+
+
+    imageElement.src = track.album.images[0].url;
+
+
+
+
+    albumCoverContainer.appendChild(imageElement);
+    trackDisplay.appendChild(albumCoverContainer);
+    trackDisplay.appendChild(trackInfoContainer);
+    trackDisplayContainer.appendChild(trackDisplay);
+    playerContainer.prepend(trackDisplayContainer)
+    musicDisplayContainer.appendChild(playerContainer)
+  }));
 }
 
-const fetchedData = async () => {
-  const tracksDataManage = await tracksData();
-
-  return tracksDataManage.map((track) => {
-    const imagesArray = track.album.images;
-    return trackImage(imagesArray[0].url)
-  });
+const spotifyTracksData = async () => {
+  const fetchedData = await tracksData();
+  const tracksArray = fetchedData.map((tracks => {
+    return tracks
+  }));
+  trackPlayer(tracksArray)
 }
 
-fetchedData()
+spotifyTracksData()
 
 
