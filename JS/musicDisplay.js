28180897f328740e13355bab937cc45d6b2b9074
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2021-05-13 19:22:24 -0700

    html snippet fro audio components has been refactored. DOM events have been added to handle seekbar, volume and play & pause functionality.

--- JS/musicDisplay.js
+++ JS/musicDisplay.js
@@ -1,16 +1,92 @@
 import { tracksData } from './spotifyDataReq.js'
 import { trackComponents } from './trackComponents.js'
 
-const spotifyTracksData = async () => {
+(async function spotifyTracksData() {
   const fetchedData = await tracksData();
   const tracksArray = fetchedData.map((tracks => {
     return tracks
   }));
-  trackComponents(tracksArray)
-}
+  trackComponents(tracksArray);
 
-spotifyTracksData()
+  /*====================================================================================*/
 
+  function trackDataLogic(num) {
+    const audioElement = document.querySelector(`.audio-${num} `);
+    const seekBarInput = document.querySelector(`.seekbar-${num}`);
+    const volumeInput = document.querySelector(`.volume-input-${num}`);
+    const playBtn = document.querySelector(`.btn-${num}`);
+    const audio = document.querySelector(`.audio-${num}`);
+    const trackDuration = document.querySelector(`.track-duration-${num}`);
+
+
+    const btnLogic = () => {
+      playBtn.addEventListener('click', () => {
+        playBtn.children[0].classList.toggle('fa-play');
+        playBtn.children[0].classList.toggle('fa-pause');
+        if (audio.paused) {
+          audio.play();
+        } else {
+          audio.pause();
+        };
+      });
+    };
+
+    const audioElementLogic = () => {
+      audioElement.addEventListener('timeupdate', () => {
+        let seconds = parseInt(audioElement.currentTime % 60);
+        let minutes = parseInt((audioElement.currentTime / 60) % 60);
+        trackDuration.innerHTML = minutes + ':' + seconds;
+      }, false)
+
+      if (audioElement.ended) {
+        playBtn.classList.remove('fa-pause');
+        playBtn.classList.add('fa-play');
+      };
+
+      audioElement.addEventListener('timeupdate', () => {
+        seekBarInput.value = audioElement.currentTime
+      });
+
+      audioElement.addEventListener('durationchange', () => {
+        seekBarInput.min = 0;
+        seekBarInput.max = audioElement.duration;
+        seekBarInput.value = 0;
+      });
+    };
+
+    const seekBarLogic = () => {
+      seekBarInput.addEventListener('input', () => {
+        audioElement.currentTime = seekBarInput.value;
+      });
+    };
+
+    const volumeControlLogic = () => {
+      volumeInput.addEventListener('change', () => {
+        audioElement.volume = volumeInput.value / 100;
+      });
+    }
+
+    btnLogic();
+    audioElementLogic();
+    seekBarLogic();
+    volumeControlLogic();
+  }
+
+  trackDataLogic(0)
+  trackDataLogic(1)
+  trackDataLogic(2)
+  trackDataLogic(3)
+  trackDataLogic(4)
+  trackDataLogic(5)
+  trackDataLogic(6)
+  trackDataLogic(7)
+  trackDataLogic(8)
+  trackDataLogic(9)
+  trackDataLogic(10)
+  trackDataLogic(11)
+  trackDataLogic(12)
+
+})()
 
 
 
--- JS/trackComponents.js
+++ JS/trackComponents.js
@@ -1,53 +1,75 @@
 const musicDisplayContainer = document.querySelector('.music-display');
 
 export const trackComponents = (trackInfo) => {
-  trackInfo.map((track => {
-    const trackContainer = document.createElement('div');
+  console.log(trackInfo)
 
-    const audioVerification = () => {
-      if (track.preview_url !== null) {
-        return track.preview_url
-      }
-    }
+  function trackContainer(num, genre, audioLink) {
+    let trackContainer = document.createElement('div');
 
-    console.log(track.preview_url)
     trackContainer.innerHTML = ` 
-      <div class="track">
+      <div class="track ${genre}">
         <div class="track-display">
           <div class="album-container">
-            <img class='album-cover' src=${track.album.images[0].url} alt="music-albums">
+            <img class='album-cover' src=${trackInfo[num].album.images[0].url} alt="music-albums">
           </div>
-          <div class="track-information hide-information">
+          <div class="track-information">
             <ul class="track-info">
-              <li class="track-name">${track.name}</li>
-              <li class="artist">${track.artists[0].name}</li>
-              <li><span id="trackDuration"></span><i> / </i>:30</li>
+              <li class="track-name">${trackInfo[num].name}</li>
+              <li class="artist">${trackInfo[num].artists[0].name}</li>
+              <li><span class='track-duration-${num}'></span><i> / </i>:30</li>
             </ul>
             <div class="track-controls">
-              <div id="playBtn">
+              <div class="play-btn btn-${num}">
                 <i class="fas fa-play"></i>
               </div>
               <div class="track-seek-container">
-                <input id='trackSeekBar' min='0' max='334' step="1" value="0" type="range" oninput="trackSeekBar()"
-                  onchange="this.inputChange">
+                <input class='seekbar-${num}' min='0' max='334' step="1" value="0" type="range">
               </div>
               <div class="volume-control">
                 <div class="volume-logo">
                   <i class="fas fa-volume-up"></i>
                 </div>
-                <input type="range" id='volumeControl' min="0" max='100' value='100' step="1"
-                  oninput="audioElement.volume = this.value/100" onchange="this.oninput()">
+                <input type="range" class='volume-input-${num}' min="0" max='100' value='100' step="1">
               </div>
             </div>
           </div>
         </div>
     </div>
 
-    <audio id="audio-container" ontimeupdate="seekBar()" ondurationchange="CreateTrackSeekBar()" preload="auto">
-      <source src=${audioVerification()} type="audio/mpeg" />
-   </audio>`;
+    <audio class="audio-container audio-${num}" preload="auto">
+      <source src=${audioLink} type="audio/mpeg" />
+    </audio>`;
 
-    musicDisplayContainer.append(trackContainer);
+    return trackContainer
+  }
 
-  }));
+  const track0 = trackContainer(0, 'instrumental', trackInfo[0].preview_url);
+  const track1 = trackContainer(1, 'edm', trackInfo[1].preview_url);
+  const track2 = trackContainer(2, 'alternative', trackInfo[2].preview_url);
+  const track3 = trackContainer(3, 'alternative', 'https://p.scdn.co/mp3-preview/c2f1314f0a573853284ac647f79f2a2c9f33b3e0?cid=774b29d4f13844c495f206cafdad9c86');
+  const track4 = trackContainer(4, 'instrumental', trackInfo[4].preview_url);
+  const track5 = trackContainer(5, 'edm', trackInfo[5].preview_url);
+  const track6 = trackContainer(6, 'instrumental', 'https://p.scdn.co/mp3-preview/77c3e50db4871d80c25f140047625a22872a2f1b?cid=774b29d4f13844c495f206cafdad9c86');
+  const track7 = trackContainer(7, 'alternative', trackInfo[7].preview_url);
+  const track8 = trackContainer(8, 'alternative', trackInfo[8].preview_url);
+  const track9 = trackContainer(9, 'pop', trackInfo[9].preview_url);
+  const track10 = trackContainer(10, 'latin', 'https://p.scdn.co/mp3-preview/3d8d79202be436eee3e6b5c2c416a6d049c56375?cid=774b29d4f13844c495f206cafdad9c86');
+  const track11 = trackContainer(11, 'pop', 'https://p.scdn.co/mp3-preview/159fd40414b8b8ac778112e41e9401de350722a5?cid=774b29d4f13844c495f206cafdad9c86');
+  const track12 = trackContainer(12, 'instrumental', 'https://p.scdn.co/mp3-preview/d54467617ec886a0d4ea70c22f270c9cc90eb74c?cid=774b29d4f13844c495f206cafdad9c86');
+
+  musicDisplayContainer.append(
+    track0,
+    track1,
+    track2,
+    track3,
+    track4,
+    track5,
+    track6,
+    track7,
+    track8,
+    track9,
+    track10,
+    track11,
+    track12,
+  );
 }
